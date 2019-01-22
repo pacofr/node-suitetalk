f63bb3762874cc953fe2803a09afc7c3b49c8fcc
pacofr <pascal.milliez@gmail.com>
2019-01-22 10:30:28 -0500

    Fix searchBooleanField - operator should not be sent

--- app/search/Fields/searchBooleanField.js
+++ app/search/Fields/searchBooleanField.js
@@ -9,6 +9,44 @@ class SearchBooleanField extends SearchField {
         this._type = "platformCore";
         this._name = "SearchBooleanField";
     }
+
+    _getAttributes() {
+        // operator is not needed
+        return {
+            "xsi:type": `${this._type}:${this._name}`,
+        };
+    }
+
+    getNode() {
+
+        const attributes = this._getAttributes();
+        const type = this._getSoapType();
+
+        if (!type) {
+            throw new Error(`Invalid SOAP type ${type}`);
+        }
+
+        if (!this.field) {
+            throw new Error("search criteria field not set");
+        }
+
+        if (!this.searchValue) {
+            throw new Error("search criteria searchValue not set");
+        }
+
+        const node = {};
+
+        node[type] = {};
+
+        if (attributes) {
+            node[type]["$attributes"] = attributes;
+        }
+
+        node[type]["platformCore:searchValue"] = {};
+        node[type]["platformCore:searchValue"]["$attributes"] = {};
+        node[type]["platformCore:searchValue"]["$attributes"][this.field] = this.searchValue;
+        return node;
+    }
 }
 
 module.exports = SearchBooleanField;
